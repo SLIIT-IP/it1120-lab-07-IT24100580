718820254c7eecebbe6b812aecdb9fd20a18706d
IT24100580 <it24100580@my.sliit.lk>
2024-09-21 07:34:30 -0700

    add to IT24100580Lab7Q2A

---
+++ Q2/IT24100580Lab7Q2A.java
@@ -0,0 +1,8 @@
+public class IT24100580Lab7Q2A {
+    public static void main(String[] args) {
+      
+        for (int i = 0; i < 4; i++) {
+            System.out.println("$ $ $ $ $");
+        }
+    }
+}
